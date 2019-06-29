d88d78fd94a9465f40b66a09c627e3f6bd16da6d
prerit1998jain <preritjain1234@gmail.com>
2019-06-30 01:58:37 +0530

    Added

--- Sorting Algorithms /maxToys.py
+++ Sorting Algorithms /maxToys.py
@@ -1,3 +1,11 @@
+'''
+
+Mark and Jane are very happy after having their first child.
+Their son loves toys, so Mark wants to buy some. There are a number of different
+toys lying in front of him, tagged with their prices. Mark has only a certain
+amount to spend, and he wants to maximize the number of toys he buys with this money.
+
+'''
 import math
 import os
 import random
