bdfa5b82aaf6cc4c73a6b2d54ae4a1ccbbad5cdb
prerit1998jain <preritjain1234@gmail.com>
2019-06-30 02:45:23 +0530

    COMPLETED

--- Sorting Algorithms /Bubble_Sort_Recursive.py
+++ Sorting Algorithms /Bubble_Sort_Recursive.py
@@ -6,10 +6,16 @@ n = int(input("Enter the size of array \n"))
 q = list(map(int, input("Enter the {} values separated by spaces \n".format(n)).rstrip().split()))
 
 def Bubble_sort_recursive(arr):
-    for i in range(len(arr)):
-        one_pass(arr[0:(len(arr)-i)])
-        print(arr)
+    flag = 0
+    for i in range(len(arr)-1):
+        if(arr[i]>arr[i+1]):
+            flag = 1
+            arr[i],arr[i+1]= arr[i+1],arr[i]
+    if flag == 1:
+        Bubble_sort_recursive(arr)
+
     return(arr)
+
 
 arr = Bubble_sort_recursive(q)
 print(arr)
