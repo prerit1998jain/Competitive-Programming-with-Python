e848510a0d2961a1bca49fff1722d96f2f51e867
prerit1998jain <preritjain1234@gmail.com>
2019-07-08 00:57:21 +0530

    Changes in comments

--- Warm-up Challenges/Counting_Valleys.py
+++ Warm-up Challenges/Counting_Valleys.py
@@ -1,11 +1,18 @@
 '''
 #### COUNTING VALLEYS PROBLEM ######
 
-Gary is an avid hiker. He tracks his hikes meticulously, paying close attention to small details like topography. During his last hike he took exactly n steps. For every step he took, he noted if it was an uphill, U , or a downhill, D step. Gary's hikes start and end at sea level and each step up or down represents a unit change in altitude. We define the following terms:
+Gary is an avid hiker. He tracks his hikes meticulously, paying close attention
+to small details like topography. During his last hike he took exactly n steps.
+For every step he took, he noted if it was an uphill, U , or a downhill, D step.
+Gary's hikes start and end at sea level and each step up or down represents a unit
+change in altitude. We define the following terms:
 
-A mountain is a sequence of consecutive steps above sea level, starting with a step up from sea level and ending with a step down to sea level.
-A valley is a sequence of consecutive steps below sea level, starting with a step down from sea level and ending with a step up to sea level.
-Given Gary's sequence of up and down steps during his last hike, find and print the number of valleys he walked through.
+A mountain is a sequence of consecutive steps above sea level, starting with a step
+up from sea level and ending with a step down to sea level. A valley is a sequence
+of consecutive steps below sea level, starting with a step down from sea level
+and ending with a step up to sea level.
+Given Gary's sequence of up and down steps during his last hike, find and print
+the number of valleys he walked through.
 
 '''
 
@@ -26,12 +33,12 @@ def countingValleys(n, s):
     #cons_D = 0
     count = 0
     for i in range(n):
-        # Cheking sequentially the string Character and 
+        # Cheking sequentially the string Character and
         # determining the sea level after each step
         if(s[i] == 'U'):
             value += 1
             steps.append(value)
-    
+
         else:
             value -= 1
             steps.append(value)
