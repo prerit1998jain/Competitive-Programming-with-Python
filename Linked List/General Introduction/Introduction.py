7919c3efa37a3f6069745fb6cf49861746e08175
prerit1998jain <preritjain1234@gmail.com>
2019-07-19 00:37:53 +0300

    Added Introductory code

---
+++ Linked List/General Introduction/Introduction.py
@@ -0,0 +1,22 @@
+''' Introduction to the LinkedList '''
+
+'''
+A linked list is a sequence of elements, which are connected together via links.
+Each data element contains a connection to another data element in form of a
+pointer.
+
+Python does not have linked lists in its standard library. We implement the concept
+of linked lists using the concept of nodes. We can create an object 'node' which
+has following attributes, it stores a data element, and has a pointer to the next
+node.
+
+Let us first start with implementing the singly linked lists. IN this type of
+data structures there is only one link between any two nodes or data containing
+elements. We create such a list and create additional methods to insert, update
+and remove elements from the list.
+
+'''
+
+'''
+CREATION OF THE LINKED LIST.
+'''
